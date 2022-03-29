80e7179799dc3cbcbabd8cdd6ab902c6fc7a110a
Jason McEwen <jason.mcewen@gmail.com>
2022-03-29 16:24:22 +0100

    First implementation of inverse sov

--- s2fft/transform.py
+++ s2fft/transform.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.fft as fft
 import s2fft.sampling as samples
 import s2fft.wigner as wigner
 
@@ -41,6 +42,117 @@ def inverse_direct(
                             * dl[m + L - 1, -spin + L - 1]
                             * flm[i]
                         )
+
+    return f
+
+
+def inverse_sov(
+    flm: np.ndarray, L: int, spin: int = 0, sampling: str = "mw"
+) -> np.ndarray:
+
+    # TODO: Check flm shape consistent with L
+
+    ntheta = samples.ntheta(L, sampling)
+    nphi = samples.nphi_equiang(L, sampling)
+    f = np.zeros((ntheta, nphi), dtype=np.complex128)
+
+    thetas = samples.thetas(L, sampling)
+    phis_equiang = samples.phis_equiang(L, sampling)
+
+    dl = np.zeros((2 * L - 1, 2 * L - 1), dtype=np.float64)
+
+    fmt = np.zeros((2 * L - 1, ntheta), dtype=np.complex128)
+    for t, theta in enumerate(thetas):
+
+        for el in range(0, L):
+
+            dl = wigner.risbo.compute_full(dl, theta, L, el)
+
+            if el >= np.abs(spin):
+
+                elfactor = np.sqrt((2 * el + 1) / (4 * np.pi))
+
+                for m in range(-el, el + 1):
+
+                    i = samples.elm2ind(el, m)
+
+                    fmt[m + L - 1, t] += (
+                        (-1) ** spin * elfactor * dl[m + L - 1, -spin + L - 1] * flm[i]
+                    )
+
+    for t, theta in enumerate(thetas):
+
+        for p, phi in enumerate(phis_equiang):
+
+            for m in range(-(L - 1), L):
+
+                f[t, p] += fmt[m + L - 1, t] * np.exp(1j * m * phi)
+
+    return f
+
+
+def inverse_sov_fft(
+    flm: np.ndarray, L: int, spin: int = 0, sampling: str = "mw"
+) -> np.ndarray:
+
+    # TODO: Check flm shape consistent with L
+
+    ntheta = samples.ntheta(L, sampling)
+    nphi = samples.nphi_equiang(L, sampling)
+    f = np.zeros((ntheta, nphi), dtype=np.complex128)
+
+    thetas = samples.thetas(L, sampling)
+    phis_equiang = samples.phis_equiang(L, sampling)
+
+    dl = np.zeros((2 * L - 1, 2 * L - 1), dtype=np.float64)
+
+    fmt = np.zeros((2 * L - 1, ntheta), dtype=np.complex128)
+    for t, theta in enumerate(thetas):
+
+        for el in range(0, L):
+
+            dl = wigner.risbo.compute_full(dl, theta, L, el)
+
+            if el >= np.abs(spin):
+
+                elfactor = np.sqrt((2 * el + 1) / (4 * np.pi))
+
+                for m in range(-el, el + 1):
+
+                    i = samples.elm2ind(el, m)
+
+                    fmt[m + L - 1, t] += (
+                        (-1) ** spin * elfactor * dl[m + L - 1, -spin + L - 1] * flm[i]
+                    )
+
+    fmt_shift = np.zeros((2 * L - 1, ntheta), dtype=np.complex128)
+    for t, theta in enumerate(thetas):
+
+        fmt_shift[0:L, t] = fmt[0 + L - 1 : L + L - 1, t]
+        fmt_shift[L : 2 * L - 1, t] = fmt[-(L - 1) + L - 1 : 0 + L - 1, t]
+
+        # f[t, :] = fft.ifft(fmt_shift[:, t], norm="forward")  # * 2 * np.pi / (2 * L - 1)
+
+        # for p, phi in enumerate(phis_equiang):
+
+        #     for m in range(-(L - 1), L):
+        #         mp = m + L - 1
+
+        #         # f[t, p] += fmt[m + L - 1, t] * np.exp(1j * m * phi)
+        #         # f[t, p] += fmt_shift[m, t] * np.exp(1j * (mp - L + 1) * phi)
+        #         # f[t, p] += (
+        #         #     fmt_shift[m, t]
+        #         #     * np.exp(1j * mp * phi)
+        #         #     * np.exp(-1j * (L - 1) * phi)
+        #         # )
+        #         # f[t, p] += (
+        #         #     fmt[mp, t] * np.exp(1j * mp * phi) * np.exp(-1j * (L - 1) * phi)
+        #         # )
+        #         f[t, p] += fmt_shift[mp, t] * np.exp(1j * mp * phi)
+
+    # f = fft.ifft(fft.ifftshift(fmt, axes=0), axis=0, norm="forward")
+    f = fft.ifft(fmt_shift, axis=0, norm="forward")
+    f = np.transpose(f)
 
     return f
 
--- tests/test_transform.py
+++ tests/test_transform.py
@@ -4,7 +4,7 @@ import s2fft as s2f
 import pyssht as ssht
 
 
-# @pytest.mark.skip(reason="Temporarily skipped for faster development")
+@pytest.mark.skip(reason="Temporarily skipped for faster development")
 @pytest.mark.parametrize("L", [15, 16])
 @pytest.mark.parametrize("spin", [0, 2])
 @pytest.mark.parametrize("sampling", ["mw", "mwss", "dh"])
@@ -21,6 +21,39 @@ def test_transform_inverse_direct(L: int, spin: int, sampling: str):
     np.testing.assert_allclose(f, f_check, atol=1e-14)
 
 
+@pytest.mark.parametrize("L", [15])
+@pytest.mark.parametrize("spin", [0])
+@pytest.mark.parametrize("sampling", ["mw"])
+def test_transform_inverse_sov(L: int, spin: int, sampling: str):
+
+    ncoeff = s2f.sampling.ncoeff(L)
+    flm = np.zeros(ncoeff, dtype=np.complex128)
+    flm = np.random.rand(ncoeff) + 1j * np.random.rand(ncoeff)
+
+    f = s2f.transform.inverse_sov(flm, L, spin, sampling)
+
+    f_check = ssht.inverse(flm, L, Method=sampling.upper(), Spin=spin, Reality=False)
+
+    np.testing.assert_allclose(f, f_check, atol=1e-14)
+
+
+@pytest.mark.parametrize("L", [15])
+@pytest.mark.parametrize("spin", [0])
+@pytest.mark.parametrize("sampling", ["mw"])
+def test_transform_inverse_sov_fft(L: int, spin: int, sampling: str):
+
+    ncoeff = s2f.sampling.ncoeff(L)
+    flm = np.zeros(ncoeff, dtype=np.complex128)
+    flm = np.random.rand(ncoeff) + 1j * np.random.rand(ncoeff)
+
+    f = s2f.transform.inverse_sov_fft(flm, L, spin, sampling)
+
+    f_check = ssht.inverse(flm, L, Method=sampling.upper(), Spin=spin, Reality=False)
+
+    np.testing.assert_allclose(f, f_check, atol=1e-14)
+
+
+@pytest.mark.skip(reason="Temporarily skipped for faster development")
 @pytest.mark.parametrize("L", [15, 16])
 @pytest.mark.parametrize("spin", [0, 2])
 @pytest.mark.parametrize("sampling", ["dh"])
