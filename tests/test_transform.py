69b149e459f0ce38a2d68edbca8776218be1211e
CosmoMatt <mattaprice@hotmail.co.uk>
2022-03-30 09:49:57 +0100

    add ssht complex signal generator fixture to pytests

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -3,20 +3,16 @@ import numpy as np
 import s2fft as s2f
 import pyssht as ssht
 
+from .test_helper_functions import *
 
 # @pytest.mark.skip(reason="Temporarily skipped for faster development")
 @pytest.mark.parametrize("L", [15, 16])
 @pytest.mark.parametrize("spin", [0, 2])
 @pytest.mark.parametrize("sampling", ["mw", "mwss", "dh"])
-def test_transform_inverse_direct(L: int, spin: int, sampling: str):
+def test_transform_inverse_direct(signal_generator, L: int, spin: int, sampling: str):
 
-    ncoeff = s2f.sampling.ncoeff(L)
-    flm = np.zeros(ncoeff, dtype=np.complex128)
-    flm = np.random.rand(ncoeff) + 1j * np.random.rand(ncoeff)
-
+    f_check, flm = signal_generator(L=L,  Method=sampling.upper(), Spin=spin, Reality=False)
     f = s2f.transform.inverse_direct(flm, L, spin, sampling)
-
-    f_check = ssht.inverse(flm, L, Method=sampling.upper(), Spin=spin, Reality=False)
 
     np.testing.assert_allclose(f, f_check, atol=1e-14)
 
@@ -24,7 +20,7 @@ def test_transform_inverse_direct(L: int, spin: int, sampling: str):
 @pytest.mark.parametrize("L", [15, 16])
 @pytest.mark.parametrize("spin", [0, 2])
 @pytest.mark.parametrize("sampling", ["dh"])
-def test_transform_forward_direct(L: int, spin: int, sampling: str):
+def test_transform_forward_direct(signal_generator, L: int, spin: int, sampling: str):
 
     # TODO: move this and potentially do better
     np.random.seed(2)
