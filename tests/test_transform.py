a63bd5c89e386171a4de01d9b6a246a35fb370f3
Jason McEwen <jason.mcewen@gmail.com>
2022-03-31 12:25:39 +0100

    Speed up transform tests (use ssht for all base cases)

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -10,7 +10,6 @@ L_to_test = [3, 4, 5]
 spin_to_test = [0, 1, 2]
 
 
-@pytest.mark.skip(reason="Temporarily skipped for faster development")
 @pytest.mark.parametrize("L", L_to_test)
 @pytest.mark.parametrize("spin", spin_to_test)
 @pytest.mark.parametrize("sampling", ["mw", "mwss", "dh"])
@@ -23,7 +22,6 @@ def test_transform_inverse_direct(flm_generator, L: int, spin: int, sampling: st
     np.testing.assert_allclose(f, f_check, atol=1e-14)
 
 
-@pytest.mark.skip(reason="Temporarily skipped for faster development")
 @pytest.mark.parametrize("L", L_to_test)
 @pytest.mark.parametrize("spin", spin_to_test)
 @pytest.mark.parametrize("sampling", ["mw", "mwss", "dh"])
@@ -38,7 +36,6 @@ def test_transform_inverse_sov(flm_generator, L: int, spin: int, sampling: str):
     np.testing.assert_allclose(f, f_check, atol=1e-14)
 
 
-@pytest.mark.skip(reason="Temporarily skipped for faster development")
 @pytest.mark.parametrize("L", L_to_test)
 @pytest.mark.parametrize("spin", spin_to_test)
 @pytest.mark.parametrize("sampling", ["mw", "mwss", "dh"])
@@ -53,7 +50,6 @@ def test_transform_inverse_sov_fft(flm_generator, L: int, spin: int, sampling: s
     np.testing.assert_allclose(f, f_check, atol=1e-14)
 
 
-@pytest.mark.skip(reason="Temporarily skipped for faster development")
 @pytest.mark.parametrize("L", L_to_test)
 @pytest.mark.parametrize("spin", spin_to_test)
 @pytest.mark.parametrize("sampling", ["dh"])
@@ -64,14 +60,13 @@ def test_transform_forward_direct(flm_generator, L: int, spin: int, sampling: st
 
     flm = flm_generator(L=L, spin=spin, reality=False)
 
-    f = s2f.transform.inverse_direct(flm, L, spin, sampling)
+    f = ssht.inverse(flm, L, Method=sampling.upper(), Spin=spin, Reality=False)
 
     flm_recov = s2f.transform.forward_direct(f, L, spin, sampling)
 
     np.testing.assert_allclose(flm, flm_recov, atol=1e-14)
 
 
-@pytest.mark.skip(reason="Temporarily skipped for faster development")
 @pytest.mark.parametrize("L", L_to_test)
 @pytest.mark.parametrize("spin", spin_to_test)
 @pytest.mark.parametrize("sampling", ["dh"])
@@ -82,29 +77,11 @@ def test_transform_forward_sov(flm_generator, L: int, spin: int, sampling: str):
 
     flm = flm_generator(L=L, spin=spin, reality=False)
 
-    f = s2f.transform.inverse_direct(flm, L, spin, sampling)
+    f = ssht.inverse(flm, L, Method=sampling.upper(), Spin=spin, Reality=False)
 
     flm_recov = s2f.transform.forward_sov(f, L, spin, sampling)
 
     np.testing.assert_allclose(flm, flm_recov, atol=1e-14)
-
-
-# @pytest.mark.skip(reason="Temporarily skipped for faster development")
-# @pytest.mark.parametrize("L", L_to_test)
-# @pytest.mark.parametrize("spin", spin_to_test)
-# @pytest.mark.parametrize("sampling", ["dh"])
-# def test_transform_forward_sov_fft(flm_generator, L: int, spin: int, sampling: str):
-
-#     # TODO: move this and potentially do better
-#     np.random.seed(2)
-
-#     flm = flm_generator(L=L, spin=spin, reality=False)
-
-#     f = s2f.transform.inverse_direct(flm, L, spin, sampling)
-
-#     flm_recov = s2f.transform.forward_sov_fft(f, L, spin, sampling)
-
-#     np.testing.assert_allclose(flm, flm_recov, atol=1e-14)
 
 
 @pytest.mark.parametrize("L", [5])
@@ -115,9 +92,9 @@ def test_transform_forward_sov_fft(flm_generator, L: int, spin: int, sampling: s
     # TODO: move this and potentially do better
     np.random.seed(2)
 
-    flm = flm_generator(L=L, spin=spin, reality=True)
+    flm = flm_generator(L=L, spin=spin, reality=False)
 
-    f = s2f.transform.inverse_direct(flm, L, spin, sampling)
+    f = ssht.inverse(flm, L, Method=sampling.upper(), Spin=spin, Reality=False)
 
     flm_recov = s2f.transform.forward_sov_fft(f, L, spin, sampling)
 
