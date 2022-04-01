a02bd05dc2c0cb9ea4101f97868a625c21b835d2
Jason McEwen <jason.mcewen@gmail.com>
2022-04-01 17:26:48 +0100

    Move quadrature tests into separate test file

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -88,41 +88,6 @@ def test_hp_ang2pix(nside: int):
         assert i == j
 
 
-@pytest.mark.parametrize("L", [5, 6])
-@pytest.mark.parametrize("sampling", ["mw", "mwss"])
-def test_quadrature_mw_weights(flm_generator, L: int, sampling: str):
-
-    # TODO: move this and potentially do better
-    # np.random.seed(2)
-
-    spin = 0
-
-    q = s2f.quadrature.quad_weights(L, sampling, spin)
-
-    flm = flm_generator(L, spin, reality=False)
-
-    f = s2f.transform.inverse_sov_fft(flm, L, spin, sampling)
-
-    integral = flm[0, 0 + L - 1] * np.sqrt(4 * np.pi)
-    q = np.reshape(q, (-1, 1))
-
-    nphi = s2f.samples.nphi_equiang(L, sampling)
-    Q = q.dot(np.ones((1, nphi)))
-
-    integral_check = np.sum(Q * f)
-
-    print(f"sampling = {sampling}")
-    print(f"q = {q}")
-    # print(f"Q = {Q}")
-    # print(f"q.shape = {q.shape}")
-    # print(f"Q.shape = {Q.shape}")
-
-    print(f"integral = {integral}")
-    print(f"integral_check = {integral_check}")
-
-    np.testing.assert_allclose(integral, integral_check, atol=1e-14)
-
-
 def test_samples_exceptions():
 
     L = 10
@@ -156,9 +121,3 @@ def test_samples_exceptions():
 
     with pytest.raises(ValueError) as e:
         s2f.samples.t2theta(L, 0, sampling="foo")
-
-    with pytest.raises(ValueError) as e:
-        s2f.quadrature.quad_weights_transform(L, sampling="foo")
-
-    with pytest.raises(ValueError) as e:
-        s2f.quadrature.quad_weights(L, sampling="foo")
