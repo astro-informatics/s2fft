4612aaf347195c8e58d6cbdaf1d84d59dec864d7
Matt Graham <matthew.m.graham@gmail.com>
2022-11-02 18:51:58 +0000

    Pass random number generator as argument

--- s2fft/utils.py
+++ s2fft/utils.py
@@ -2,13 +2,17 @@ import numpy as np
 import s2fft.samples as samples
 
 
-def generate_flm(L: int, spin: int = 0, reality: bool = False) -> np.ndarray:
+def generate_flm(
+    rng: np.random.Generator, L: int, spin: int = 0, reality: bool = False
+) -> np.ndarray:
     r"""Generate a 2D set of random harmonic coefficients.
 
     Note:
         Real signals are explicitly produced from conjugate symmetry.
 
     Args:
+        rng (Generator): Random number generator.
+
         L (int): Harmonic band-limit.
 
         spin (int, optional): Harmonic spin. Defaults to 0.
@@ -24,15 +28,15 @@ def generate_flm(L: int, spin: int = 0, reality: bool = False) -> np.ndarray:
     for el in range(spin, L):
 
         if reality:
-            flm[el, 0 + L - 1] = np.random.rand()
+            flm[el, 0 + L - 1] = rng.uniform()
         else:
-            flm[el, 0 + L - 1] = np.random.rand() + 1j * np.random.rand()
+            flm[el, 0 + L - 1] = rng.uniform() + 1j * rng.uniform()
 
         for m in range(1, el + 1):
-            flm[el, m + L - 1] = np.random.rand() + 1j * np.random.rand()
+            flm[el, m + L - 1] = rng.uniform() + 1j * rng.uniform()
             if reality:
                 flm[el, -m + L - 1] = (-1) ** m * np.conj(flm[el, m + L - 1])
             else:
-                flm[el, -m + L - 1] = np.random.rand() + 1j * np.random.rand()
+                flm[el, -m + L - 1] = rng.uniform() + 1j * rng.uniform()
 
     return flm
