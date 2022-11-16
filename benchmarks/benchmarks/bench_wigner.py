8a90cf9e2dc76a61c8cc2d2948aa9a8e3baa8f51
Devaraj Gopinathan <d.gopinathan@ucl.ac.uk>
2022-11-16 09:54:13 +0000

    lists input  parameters for benchmarking

--- benchmarks/benchmarks/bench_wigner.py
+++ benchmarks/benchmarks/bench_wigner.py
@@ -13,3 +13,21 @@ config.update("jax_enable_x64", True)  # this only works on startup!
 import s2fft.wigner as wigner
 import s2fft.samples as samples
 import pyssht as ssht
+
+# list of different parameters to benchmark
+par = {}
+# recursions
+# par["RECURSION"] = ["risbo", "trapani", "turok"]
+par["RECURSION"] = ["turok"]
+# data types
+# par["DATATYPE"] = ["float32", "float64"]
+par["DATATYPE"] = ["float64"]
+# implementations
+par["IMPLEMENTATION"] = ["loop", "vectorized", "jax"]
+# harmonic band-limit
+# par["BANDLIMIT"] = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+par["BANDLIMIT"] = [32, 64]
+# sampling schemes
+# par["SAMPLING"] = ["mw", "mwss", "dh", "healpix"]
+# colatitude
+par["COLATITUDE"] = [np.pi / 2.0]
