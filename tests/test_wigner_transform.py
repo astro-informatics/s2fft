a70d41a944f131c4bbf85d79980ea77f81b6a089
Jason McEwen <jason.mcewen@gmail.com>
2022-11-25 09:35:01 +0000

    Add Wigner transform round trip test (currently failing)

--- tests/test_wigner_transform.py
+++ tests/test_wigner_transform.py
@@ -6,12 +6,13 @@ import s2fft as s2f
 
 L_to_test = [8, 16]
 N_to_test = [2, 4, 6]
-sampling_schemes = ["mw", "mwss"]
+sampling_schemes_so3 = ["mw", "mwss"]
+sampling_schemes = ["mw", "mwss", "dh"]
 
 
 @pytest.mark.parametrize("L", L_to_test)
 @pytest.mark.parametrize("N", N_to_test)
-@pytest.mark.parametrize("sampling", sampling_schemes)
+@pytest.mark.parametrize("sampling", sampling_schemes_so3)
 def test_inverse_wigner_transform(
     flmn_generator, s2fft_to_so3_sampling, L: int, N: int, sampling: str
 ):
@@ -32,7 +33,7 @@ def test_inverse_wigner_transform(
 
 @pytest.mark.parametrize("L", L_to_test)
 @pytest.mark.parametrize("N", N_to_test)
-@pytest.mark.parametrize("sampling", sampling_schemes)
+@pytest.mark.parametrize("sampling", sampling_schemes_so3)
 def test_forward_wigner_transform(
     flmn_generator, s2fft_to_so3_sampling, L: int, N: int, sampling: str
 ):
@@ -53,5 +54,17 @@ def test_forward_wigner_transform(
 
     flmn_check = s2f.wigner.samples.flmn_1d_to_3d(so3.forward(f_1D, params), L, N)
     flmn = s2f.wigner.transform.forward(f_3D, L, N, sampling)
+
+    np.testing.assert_allclose(flmn, flmn_check, atol=1e-14)
+
+
+@pytest.mark.parametrize("L", L_to_test)
+@pytest.mark.parametrize("N", N_to_test)
+@pytest.mark.parametrize("sampling", sampling_schemes)
+def test_round_trip_wigner_transform(flmn_generator, L: int, N: int, sampling: str):
+
+    flmn = flmn_generator(L=L, N=N, reality=False)
+    f = s2f.wigner.transform.inverse(flmn, L, N, sampling)
+    flmn_check = s2f.wigner.transform.forward(f, L, N, sampling)
 
     np.testing.assert_allclose(flmn, flmn_check, atol=1e-14)
