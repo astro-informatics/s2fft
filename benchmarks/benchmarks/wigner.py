a570deef42cf16443dbc510c3ccf7bb137191934
Devaraj Gopinathan <d.gopinathan@ucl.ac.uk>
2022-12-14 12:10:01 +0000

    shifts benchmark functions for recursions to wigner.py

--- benchmarks/benchmarks/wigner.py
+++ benchmarks/benchmarks/wigner.py
@@ -1,35 +1,6 @@
-"""
-Benchmarks for Wigner-d recursions
-"""
-
-# set threads used by numpy (before numpy is imported!)
-# import os
-# os.environ["OMP_NUM_THREADS"] = "4" # export OMP_NUM_THREADS=4
-# os.environ["OPENBLAS_NUM_THREADS"] = "4" # export OPENBLAS_NUM_THREADS=4
-# os.environ["MKL_NUM_THREADS"] = "6" # export MKL_NUM_THREADS=6
-# os.environ["VECLIB_MAXIMUM_THREADS"] = "4" # export VECLIB_MAXIMUM_THREADS=4
-# os.environ["NUMEXPR_NUM_THREADS"] = "6" # export NUMEXPR_NUM_THREADS=6
-
-
-import argparse
 import numpy as np
-
-import pyssht as ssht
 import s2fft
-import jax
-import jax.numpy as jnp
-from jax.config import config
-
-config.update("jax_enable_x64", True)  # this only works on startup!
-
-# summary of available compute resources
-# var = {}
-# var["jax_devices"] = jax.devices()
-# var["jax_local_devices"] = jax.local_devices()
-# var["jax_devices_cpu"] = jax.devices("cpu")
-# var["jax_device_count_cpu"] = jax.device_count("cpu")
-# var['jax_devices_gpu'] = jax.devices('gpu')
-# var['jax_device_count_gpu'] = jax.device_count('gpu')
+from utils import parametrize
 
 # list of different parameters to benchmark
 # harmonic band-limit
@@ -38,9 +9,6 @@ L_VALUES = [16]
 BETA = np.pi / 2
 # harmonic order
 MM = 0
-
-from utils import parametrize, parameters_string, run_benchmarks
-
 
 # Risbo
 @parametrize({"L": L_VALUES})
@@ -73,7 +41,7 @@ def trapani_compute_full_vectorized(L):
     dl = np.zeros((2 * L - 1, 2 * L - 1))
     dl = s2fft.wigner.trapani.init(dl, L, "vectorized")
     for el in range(1, L):
-        dl = s2fft.wigner.trapani.compute(dl, L, el, "vectorized")
+        dl = s2fft.wigner.trapani.compute_full(dl, L, el, "vectorized")
 
 
 @parametrize({"L": L_VALUES})
@@ -119,24 +87,3 @@ def turok_jax_compute_slice_largest_plane(L):
     Turok: compute slice of wigner-d plane for L - 1 at MM (jax)
     """
     dl = s2fft.wigner.turok_jax.compute_slice(BETA, L - 1, L, MM).block_until_ready()
-
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser("Run Wigner recursion benchmarks")
-    parser.add_argument("--number-runs", type=int, default=10)
-    parser.add_argument("--number-repeats", type=int, default=3)
-    args = parser.parse_args()
-
-    results = run_benchmarks(
-        benchmarks=[
-            trapani_compute_full_sequential,
-            risbo_compute_full_sequential,
-            turok_compute_full_sequential,
-            turok_compute_full_largest_plane,
-            turok_compute_slice_largest_plane,
-            turok_jax_compute_slice_largest_plane,
-        ],
-        number_runs=args.number_runs,
-        number_repeats=args.number_repeats,
-    )
