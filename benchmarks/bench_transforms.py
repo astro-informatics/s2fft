c1d58fb93dce456a0e4dc5a745d491398e21d957
Devaraj Gopinathan <36169767+Devaraj-G@users.noreply.github.com>
2022-12-22 16:19:58 +0000

    removes comments on numpy environmnet variables
    Co-authored-by: Matt Graham <matthew.m.graham@gmail.com>

--- benchmarks/bench_transforms.py
+++ benchmarks/bench_transforms.py
@@ -2,13 +2,6 @@
 Benchmarks for transforms
 """
 
-# set threads used by numpy (before numpy is imported!)
-# import os
-# os.environ["OMP_NUM_THREADS"] = "4" # export OMP_NUM_THREADS=4
-# os.environ["OPENBLAS_NUM_THREADS"] = "4" # export OPENBLAS_NUM_THREADS=4
-# os.environ["MKL_NUM_THREADS"] = "6" # export MKL_NUM_THREADS=6
-# os.environ["VECLIB_MAXIMUM_THREADS"] = "4" # export VECLIB_MAXIMUM_THREADS=4
-# os.environ["NUMEXPR_NUM_THREADS"] = "6" # export NUMEXPR_NUM_THREADS=6
 
 import argparse
 import numpy as np
