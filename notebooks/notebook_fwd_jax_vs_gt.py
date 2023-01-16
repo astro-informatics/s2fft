bcaa4e2bad8944823b192bf90ce6d21e4b9e9050
sfmig <33267254+sfmig@users.noreply.github.com>
2023-01-16 12:49:55 +0000

    refactored notebook to check against ground truth

--- notebooks/notebook_fwd_jax_vs_gt.py
+++ notebooks/notebook_fwd_jax_vs_gt.py
@@ -3,23 +3,13 @@ import sys
 
 sys.path.append("../")
 
-import healpy as hpy
-import jax
-import jax.numpy as jnp
-import matplotlib.pyplot as plt
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-import numpy as np
-import pyssht as ssht
-from jax import device_put, jit
-from jax.config import config
 
+import numpy as np
+from jax.config import config
+import pyssht as ssht
 import s2fft as s2f
-import s2fft.healpix_ffts as hp
-import s2fft.quadrature as quadrature
-import s2fft.resampling as resampling
-import s2fft.samples as samples
-import s2fft.wigner as wigner
 
 config.update("jax_enable_x64", True)
 
@@ -28,66 +18,68 @@ config.update("jax_enable_x64", True)
 
 # generate spherical harmonics (ground truth)
 DEFAULT_SEED = 8966433580120847635
-rn_gen = np.random.default_rng(DEFAULT_SEED) # modify to use JAX random key approach?
+rn_gen = np.random.default_rng(DEFAULT_SEED)  # modify to use JAX random key approach?
 
+### input params
+L_in = 5  # input L, redefined to 2*nside if sampling is healpix
+spin = 0
+reality = True
+L_lower = 2
+nside_healpix = 2
 
 # list jax implementations
-list_jax_str = [ 
-        "jax_vmap_double",#  "jax_vmap_double_mmg",
-        "jax_vmap_scan",
-        "jax_vmap_scan_0",
-        "jax_vmap_loop",
-        "jax_vmap_loop_0",
-        "jax_map_double",
-        "jax_map_scan"
+list_jax_str = [
+    "jax_vmap_double",
+    "jax_vmap_scan",
+    "jax_vmap_loop",
+    "jax_map_double",
+    "jax_map_scan",
 ]
 
 # list of sampling approaches
 list_sampling = ["mw", "mwss", "dh", "healpix"]
 
+# Print inputs common to all sampling methods
+print("-----------------------------------------")
+print("Input params:")
+print(
+    f"L_in = {L_in}, spin = {spin}, reality = {reality}, L_lower = {L_lower}, nside_healpix={nside_healpix}"
+)
+print("-----------------------------------------")
 
-### All JAX methods
-for m_str in list_jax_str: 
+# All JAX methods
+for m_str in list_jax_str:
 
     for sampling in list_sampling:
 
-        ### input params
-        L = 5  
-        spin = 1 
-        reality = True 
-        L_lower = 2  
-        # only applicable to healpix: 
-        nside = 2 # (set to None if sampling not healpix)
-        if nside:
-            L = 2 * nside  
+        # Groundtruth and starting point f
+        if sampling != "healpix":
+            # Set nside to None if not healpix
+            nside = None
+            L = L_in
 
-        ### Groundtruth and starting point f
-        if sampling!='healpix':
-            ## Set nside to None if not healpix
-            nside=None
-
-            # compute signal in time domain with ssht (starting point)
-            flm_gt = s2f.utils.generate_flm(
-                rn_gen, L, L_lower, spin, reality=reality
-            )  # shape L, 2L-1
-
+            # compute ground truth and starting point f
+            flm_gt = s2f.utils.generate_flm(rn_gen, L, L_lower, spin, reality=reality)
             f = ssht.inverse(
-                s2f.samples.flm_2d_to_1d(flm_gt, L),  # 2D indexed coeffs to 1D indexed
+                s2f.samples.flm_2d_to_1d(flm_gt, L),
                 L,
                 Method=sampling.upper(),
                 Spin=spin,
                 Reality=False,
             )
+
         else:
-            flm_gt0 = s2f.utils.generate_flm(
-                rn_gen, L, L_lower, spin, reality=reality
-            )  # shape L, 2L-1
+            # Set nside to healpix value and redefine L
+            nside = nside_healpix
+            L = 2 * nside  # L redefined to double nside
+
+            # compute ground truth and starting point f
+            flm_gt0 = s2f.utils.generate_flm(rn_gen, L, L_lower, spin, reality=reality)
             f = s2f.transform._inverse(
                 flm_gt0, L, sampling=sampling, method="direct", nside=nside
             )
 
-        ### Compute numpy solution if sampling is healpix (used as GT)
-        if sampling=='healpix': 
+            # Compute numpy solution if sampling is healpix (used as GT)
             flm_sov_fft_vec = s2f.transform._forward(
                 f,
                 L,
@@ -96,9 +88,10 @@ for m_str in list_jax_str:
                 method="sov_fft_vectorized",
                 nside=nside,
                 reality=reality,
-                L_lower=L_lower,)
-   
-        ### JAX implementation
+                L_lower=L_lower,
+            )
+
+        # JAX implementation
         flm_sov_fft_vec_jax = s2f.transform._forward(
             f,
             L,
@@ -110,12 +103,19 @@ for m_str in list_jax_str:
             L_lower=L_lower,
         )
 
-        if sampling=='healpix': # compare to numpy result rather than GT
-            print(f'{m_str} vs numpy ({sampling}): {np.allclose(flm_sov_fft_vec, flm_sov_fft_vec_jax, atol=1e-14)}') 
+        # Compare to GT
+        if sampling == "healpix":  # compare to numpy result rather than GT
+            print(
+                f"{m_str} vs numpy ({sampling}, L = {L}): {np.allclose(flm_sov_fft_vec, flm_sov_fft_vec_jax, atol=1e-14)}"
+            )
         else:
-            print(f'{m_str} vs GT ({sampling}): {np.allclose(flm_gt, flm_sov_fft_vec_jax, atol=1e-14)}') 
+            print(
+                f"{m_str} vs GT ({sampling}, L = {L}): {np.allclose(flm_gt, flm_sov_fft_vec_jax, atol=1e-14)}"
+            )
 
         # %timeit s2f.transform._forward(f, L, spin, sampling, method=m_str, nside=nside, reality=reality, L_lower=L_lower)
         # %timeit s2f.transform._forward(f, L, spin, sampling, method=m_str, nside=nside, reality=reality, L_lower=L_lower)
-    print('----')
+    print("-----------------------------------------")
 
+
+# %%
