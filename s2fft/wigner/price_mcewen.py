d39c9378411b2940f9594215b48b7eff2b26a889
CosmoMatt <mattaprice@hotmail.co.uk>
2023-01-28 17:22:09 +0000

    explicitly remove pole singularity for higher L

--- s2fft/wigner/price_mcewen.py
+++ s2fft/wigner/price_mcewen.py
@@ -2,7 +2,7 @@ import numpy as np
 from s2fft import samples
 
 
-def compute_vectorised_slice(beta: np.ndarray, L: int, mm: int) -> np.ndarray:
+def generate_precomputes(beta: np.ndarray, L: int, mm: int) -> np.ndarray:
     ntheta = len(beta)  # Number of theta samples
     el = np.arange(L)
     nel = len(el)  # Number of harmonic modes.
@@ -18,7 +18,6 @@ def compute_vectorised_slice(beta: np.ndarray, L: int, mm: int) -> np.ndarray:
 
     # Indexing boundaries
     half_slices = [el + mm + 1, el - mm + 1]
-    lims = [0, -1]
 
     # Vectors with indexing -L < m < L adopted throughout
     cpi = np.zeros((L + 1, nel), dtype=np.float64)
@@ -62,7 +61,24 @@ def compute_vectorised_slice(beta: np.ndarray, L: int, mm: int) -> np.ndarray:
                 lamb[i, j, k] -= (nel - k - 1) * cs[j]
                 lrenorm[i, j, k] = log_first_row[half_slices[i][k] - 1, j, k]
 
+    return [lrenorm, lamb, vsign, cpi, cp2]
+
+
+def compute_vectorised_slice(beta: np.ndarray, L: int, mm: int) -> np.ndarray:
+    ntheta = len(beta)  # Number of theta samples
+    el = np.arange(L)
+    nel = len(el)  # Number of harmonic modes.
+
+    # Trigonometric constant adopted throughout
+    c = np.cos(beta)
+    s = np.sin(beta)
+    cs = c / s
+
+    # Indexing boundaries
+    lims = [0, -1]
+
     dl_test = np.zeros((2 * L - 1, ntheta, nel), dtype=np.float64)
+    lrenorm, lamb, vsign, cpi, cp2 = generate_precomputes(beta, L, mm)
 
     for i in range(2):
         lind = L - 1
@@ -129,56 +145,10 @@ def latitudinal_step(
     c = np.cos(beta)
     s = np.sin(beta)
     cs = c / s
-    t = np.tan(-beta / 2.0)
-    lt = np.log(np.abs(t))
-    c2 = np.cos(beta / 2.0)
-    omc = 1.0 - c
 
     # Indexing boundaries
-    half_slices = [el + mm + 1, el - mm + 1]
     lims = [0, -1]
-
-    # Vectors with indexing -L < m < L adopted throughout
-    cpi = np.zeros((L + 1, nel), dtype=np.float64)
-    cp2 = np.zeros((L + 1, nel), dtype=np.float64)
-    log_first_row = np.zeros((2 * L + 1, ntheta, nel), dtype=np.float64)
-
-    # Populate vectors for first row
-    log_first_row[0] = np.einsum("l,t->tl", 2.0 * el, np.log(np.abs(c2)))
-
-    for i in range(2, L + abs(mm) + 2):
-        ratio = (2 * el + 2 - i) / (i - 1)
-        for j in range(ntheta):
-            log_first_row[i - 1, j] = (
-                log_first_row[i - 2, j] + np.log(ratio) / 2 + lt[j]
-            )
-
-    # Initialising coefficients cp(m)= cplus(l-m).
-    cpi[0] = 2.0 / np.sqrt(2 * el)
-    for m in range(2, L + 1):
-        cpi[m - 1] = 2.0 / np.sqrt(m * (2 * el + 1 - m))
-        cp2[m - 1] = cpi[m - 1] / cpi[m - 2]
-
-    for k in range(nel):
-        cpi[:, k] = np.roll(cpi[:, k], (nel - k - 1), axis=-1)
-        cp2[:, k] = np.roll(cp2[:, k], (nel - k - 1), axis=-1)
-    # Then evaluate the negative half row and reflect using
-    # Wigner-d symmetry relation.
-
-    # Perform precomputations (these can be done offline)
-    msign = np.hstack(((-1) ** (abs(np.arange(L - 1))), np.ones(L)))
-    lsign = (-1) ** abs(mm + el)
-    vsign = np.einsum("m,l->ml", msign, lsign)
-    vsign[: L - 1] *= (-1) ** abs(mm + 1)
-
-    lrenorm = np.zeros((2, ntheta, nel), dtype=np.float64)
-    lamb = np.zeros((2, ntheta, nel), np.float64)
-    for i in range(2):
-        for j in range(ntheta):
-            lamb[i, j] = ((el + 1) * omc[j] - half_slices[i] + c[j]) / s[j]
-            for k in range(nel):
-                lamb[i, j, k] -= (nel - k - 1) * cs[j]
-                lrenorm[i, j, k] = log_first_row[half_slices[i][k] - 1, j, k]
+    lrenorm, lamb, vsign, cpi, cp2 = generate_precomputes(beta, L, mm)
 
     for i in range(2):
         lind = L - 1
@@ -193,7 +163,7 @@ def latitudinal_step(
         )
 
         # Sum into transform vector
-        ftm[:, sind] = np.sum(
+        ftm[:, sind] = np.nansum(
             dl_iter[0, :, lind:]
             * vsign[sind, lind:]
             * np.exp(lrenorm[i, :, lind:])
@@ -202,7 +172,7 @@ def latitudinal_step(
         )
 
         # Sum into transform vector
-        ftm[:, sind + sgn] = np.sum(
+        ftm[:, sind + sgn] = np.nansum(
             dl_iter[1, :, lind - 1 :]
             * vsign[sind + sgn, lind - 1 :]
             * np.exp(lrenorm[i, :, lind - 1 :])
@@ -225,7 +195,7 @@ def latitudinal_step(
             dl_entry[:, 0] = 1
 
             # Sum into transform vector
-            ftm[:, sind + sgn * m] = np.sum(
+            ftm[:, sind + sgn * m] = np.nansum(
                 dl_entry
                 * vsign[sind + sgn * m, lind:]
                 * np.exp(lrenorm[i, :, lind:])
@@ -250,7 +220,7 @@ def inverse_transform(
     ftm = np.zeros(samples.ftm_shape(L, sampling), dtype=np.complex128)
 
     for t, theta in enumerate(thetas):
-        for el in range(L):
+        for el in range(abs(spin), L):
 
             dl = wigner.turok.compute_slice(theta, el, L, -spin)
             elfactor = np.sqrt((2 * el + 1) / (4 * np.pi))
@@ -272,6 +242,13 @@ def inverse_transform_new(
         "lm,l->lm", flm, np.sqrt((2 * np.arange(L) + 1) / (4 * np.pi))
     )
     ftm = latitudinal_step(flm, thetas, L, -spin, sampling)
+
+    # Remove pole singularity
+    if sampling == "mw":
+        ftm[-1] = 0
+        ftm[-1, L - 1 + spin] = np.nansum(
+            (-1) ** abs(np.arange(L) - spin) * flm[:, L - 1 + spin]
+        )
     return ftm
     # ftm *= (-1) ** spin
     # return np.fft.ifft(np.fft.ifftshift(ftm, axes=1), axis=1, norm="forward")
@@ -285,22 +262,15 @@ if __name__ == "__main__":
     warnings.filterwarnings("ignore")
 
     sampling = "mw"
-    L = 8
-    spin = 0
+    L = 64
+    spin = -3
 
     rng = np.random.default_rng(12341234515)
     flm = utils.generate_flm(rng, L, 0, spin)
-    flm_shift = np.zeros_like(flm)
-    flm_shift = flm
-
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.imshow(np.real(flm))
-    ax2.imshow(np.real(flm_shift))
-    plt.show()
 
     f = np.real(transform.inverse(flm, L, spin, sampling))
     f_test = np.real(inverse_transform(flm, L, spin, sampling))
-    f_test_2 = np.real(inverse_transform_new(flm_shift, L, spin, sampling))
+    f_test_2 = np.real(inverse_transform_new(flm, L, spin, sampling))
 
     f = f_test
     mx, mn = np.nanmax(f), np.nanmin(f)
@@ -320,12 +290,12 @@ if __name__ == "__main__":
 #     warnings.filterwarnings("ignore")
 
 #     sampling = "mw"
-#     L = 8
-#     el = L - 3
+#     L = 32
+#     el = L - 1
 #     betas = samples.thetas(L, sampling)
 #     beta_ind = int(L / 2)
 #     beta = betas[beta_ind]
-#     spin = 0
+#     spin = 2
 
 #     dl_turok = wigner.turok.compute_slice(beta, el, L, -spin)
 #     dl_price_mcewen = compute_vectorised_slice(betas, L, -spin)[:, beta_ind, el]
