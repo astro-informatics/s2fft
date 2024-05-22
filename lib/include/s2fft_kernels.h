31393f9f6e0e4dea098f7d30e75bfcbffd125e59
Wassim KABALAN <wastondev@gmail.com>
2024-05-22 19:44:17 +0200

    Add spectral extension and folding kernels

---
+++ lib/include/s2fft_kernels.h
@@ -0,0 +1,22 @@
+#ifndef _S2FFT_KERNELS_H
+#define _S2FFT_KERNELS_H
+
+#include "hresult.h"
+#include <cmath>  // has to be included before cuda/std/complex
+#include <cstddef>
+#include <cuda/std/complex>
+#include "cufft.h"
+#include <cufftXt.h>
+typedef long long int int64;
+
+namespace s2fftKernels {
+
+template <typename complex>
+HRESULT launch_spectral_folding(complex* data, complex* output, const int& nside, const int& L,
+                                cudaStream_t stream);
+template <typename complex>
+HRESULT launch_spectral_extension(complex* data, complex* output, const int& nside, const int& L,
+                                  cudaStream_t stream);
+}  // namespace s2fftKernels
+
+#endif  // _S2FFT_KERNELS_H
