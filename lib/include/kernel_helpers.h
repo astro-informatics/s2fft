5b482aed9af4fa26577caa7a8cc0b4f97ca955df
Matt Graham <matthew.m.graham@gmail.com>
2024-10-04 10:49:51 +0100

    Add headers with attributions / license details

--- lib/include/kernel_helpers.h
+++ lib/include/kernel_helpers.h
@@ -1,3 +1,30 @@
+// Adapted from code in a tutorial by Dan Foreman-Mackey
+// https://github.com/dfm/extending-jax/blob/c33869665236877a2ae281/lib/kernel_helpers.h
+//
+// Original license:
+//
+// MIT License
+//
+// Copyright (c) 2021 Dan Foreman-Mackey
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 // This header is not specific to our application and you'll probably want
 // something like this for any extension you're building. This includes the
 // infrastructure needed to serialize descriptors that are used with the
