23fb7afa57d37b451173f2e54717d5ed665bb7d8
Matt Graham <matthew.m.graham@gmail.com>
2024-12-11 16:30:23 +0000

    Test precompute transforms with iter > 0

--- tests/test_spherical_precompute.py
+++ tests/test_spherical_precompute.py
@@ -1,3 +1,4 @@
+import jax
 import numpy as np
 import pyssht as ssht
 import pytest
@@ -7,6 +8,8 @@ from s2fft.base_transforms import spherical as base
 from s2fft.precompute_transforms import construct as c
 from s2fft.precompute_transforms.spherical import forward, inverse
 from s2fft.sampling import s2_samples as samples
+
+jax.config.update("jax_enable_x64", True)
 
 # Maximum spin number at which Price-McEwen recursion is sufficiently accurate.
 # For spins > PM_MAX_STABLE_SPIN one should default to the Risbo recursion.
@@ -20,6 +23,7 @@ sampling_to_test = ["mw", "mwss", "dh", "gl"]
 reality_to_test = [True, False]
 methods_to_test = ["numpy", "jax", "torch"]
 recursions_to_test = ["price-mcewen", "risbo", "auto"]
+iter_to_test = [0, 3]
 
 
 @pytest.mark.parametrize("L", L_to_test)
@@ -221,6 +225,7 @@ def test_transform_forward(
 @pytest.mark.parametrize("reality", reality_to_test)
 @pytest.mark.parametrize("method", methods_to_test)
 @pytest.mark.parametrize("recursion", recursions_to_test)
+@pytest.mark.parametrize("iter", iter_to_test)
 def test_transform_forward_healpix(
     flm_generator,
     nside: int,
@@ -228,12 +233,13 @@ def test_transform_forward_healpix(
     reality: bool,
     method: str,
     recursion: str,
+    iter: int,
 ):
     sampling = "healpix"
     L = ratio * nside
     flm = flm_generator(L=L, reality=True)
     f = base.inverse(flm, L, 0, sampling, nside, reality)
-    flm_check = base.forward(f, L, 0, sampling, nside, reality)
+    flm_check = base.forward(f, L, 0, sampling, nside, reality, iter=iter)
 
     kfunc = (
         c.spin_spherical_kernel_jax
@@ -254,6 +260,7 @@ def test_transform_forward_healpix(
             reality,
             method,
             nside,
+            iter,
         )
         np.testing.assert_allclose(flm_recov, flm_check, atol=tol, rtol=tol)
 
@@ -271,10 +278,11 @@ def test_transform_forward_healpix(
                 reality,
                 method,
                 nside,
+                iter,
             ),
         )
     else:
-        flm_recov = forward(f, L, 0, kernel, sampling, reality, method, nside)
+        flm_recov = forward(f, L, 0, kernel, sampling, reality, method, nside, iter)
         np.testing.assert_allclose(flm_recov, flm_check, atol=tol, rtol=tol)
 
 
