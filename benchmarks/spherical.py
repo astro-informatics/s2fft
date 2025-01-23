a778204714d14c51de8dbff5b7521eb2250916ff
Matt Graham <matthew.m.graham@gmail.com>
2025-01-23 09:21:05 +0000

    Expose parameter in benchmarks to set HEALPix nside

--- benchmarks/spherical.py
+++ benchmarks/spherical.py
@@ -11,6 +11,7 @@ from s2fft.sampling import s2_samples as samples
 L_VALUES = [8, 16, 32, 64, 128, 256]
 L_LOWER_VALUES = [0]
 SPIN_VALUES = [0]
+L_TO_NSIDE_RATIO_VALUES = [2]
 SAMPLING_VALUES = ["mw"]
 METHOD_VALUES = ["numpy", "jax"]
 REALITY_VALUES = [True]
@@ -21,24 +22,30 @@ def _jax_arrays_to_numpy(precomps):
     return [np.asarray(p) for p in precomps]
 
 
-def setup_forward(method, L, L_lower, sampling, spin, reality, spmd):
+def _get_nside(sampling, L, L_to_nside_ratio):
+    return None if sampling != "healpix" else L // L_to_nside_ratio
+
+
+def setup_forward(method, L, L_lower, sampling, spin, L_to_nside_ratio, reality, spmd):
     if reality and spin != 0:
         skip("Reality only valid for scalar fields (spin=0).")
     if spmd and method != "jax":
         skip("GPU distribution only valid for JAX.")
     rng = np.random.default_rng()
     flm = s2fft.utils.signal_generator.generate_flm(rng, L, spin=spin, reality=reality)
+    nside = _get_nside(sampling, L, L_to_nside_ratio)
     f = s2fft.transforms.spherical.inverse(
         flm,
         L=L,
         spin=spin,
+        nside=nside,
         sampling=sampling,
         reality=reality,
         spmd=spmd,
         L_lower=L_lower,
     )
     precomps = generate_precomputes_jax(
-        L, spin, sampling, forward=True, L_lower=L_lower
+        L, spin, sampling, nside=nside, forward=True, L_lower=L_lower
     )
     if method == "numpy":
         precomps = _jax_arrays_to_numpy(precomps)
@@ -52,10 +59,13 @@ def setup_forward(method, L, L_lower, sampling, spin, reality, spmd):
     L_lower=L_LOWER_VALUES,
     sampling=SAMPLING_VALUES,
     spin=SPIN_VALUES,
+    L_to_nside_ratio=L_TO_NSIDE_RATIO_VALUES,
     reality=REALITY_VALUES,
     spmd=SPMD_VALUES,
 )
-def forward(f, precomps, method, L, L_lower, sampling, spin, reality, spmd):
+def forward(
+    f, precomps, method, L, L_lower, sampling, spin, L_to_nside_ratio, reality, spmd
+):
     if method == "pyssht":
         flm = pyssht.forward(f, L, spin, sampling.upper())
     else:
@@ -65,6 +75,7 @@ def forward(f, precomps, method, L, L_lower, sampling, spin, reality, spmd):
             L_lower=L_lower,
             precomps=precomps,
             spin=spin,
+            nside=_get_nside(sampling, L, L_to_nside_ratio),
             sampling=sampling,
             reality=reality,
             method=method,
@@ -75,7 +86,7 @@ def forward(f, precomps, method, L, L_lower, sampling, spin, reality, spmd):
     return flm
 
 
-def setup_inverse(method, L, L_lower, sampling, spin, reality, spmd):
+def setup_inverse(method, L, L_lower, sampling, spin, L_to_nside_ratio, reality, spmd):
     if reality and spin != 0:
         skip("Reality only valid for scalar fields (spin=0).")
     if spmd and method != "jax":
@@ -83,7 +94,12 @@ def setup_inverse(method, L, L_lower, sampling, spin, reality, spmd):
     rng = np.random.default_rng()
     flm = s2fft.utils.signal_generator.generate_flm(rng, L, spin=spin, reality=reality)
     precomps = generate_precomputes_jax(
-        L, spin, sampling, forward=False, L_lower=L_lower
+        L,
+        spin,
+        sampling,
+        nside=_get_nside(sampling, L, L_to_nside_ratio),
+        forward=False,
+        L_lower=L_lower,
     )
     if method == "numpy":
         precomps = _jax_arrays_to_numpy(precomps)
@@ -97,10 +113,13 @@ def setup_inverse(method, L, L_lower, sampling, spin, reality, spmd):
     L_lower=L_LOWER_VALUES,
     sampling=SAMPLING_VALUES,
     spin=SPIN_VALUES,
+    L_to_nside_ratio=L_TO_NSIDE_RATIO_VALUES,
     reality=REALITY_VALUES,
     spmd=SPMD_VALUES,
 )
-def inverse(flm, precomps, method, L, L_lower, sampling, spin, reality, spmd):
+def inverse(
+    flm, precomps, method, L, L_lower, sampling, spin, L_to_nside_ratio, reality, spmd
+):
     if method == "pyssht":
         f = pyssht.inverse(samples.flm_2d_to_1d(flm, L), L, spin, sampling.upper())
     else:
@@ -110,6 +129,7 @@ def inverse(flm, precomps, method, L, L_lower, sampling, spin, reality, spmd):
             L_lower=L_lower,
             precomps=precomps,
             spin=spin,
+            nside=_get_nside(sampling, L, L_to_nside_ratio),
             sampling=sampling,
             reality=reality,
             method=method,
