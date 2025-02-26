5333b691cc9ec880454f9475d4880638d70959dd
Matt Graham <matthew.m.graham@gmail.com>
2025-02-26 11:58:51 +0000

    Add utilities for wrapping JAX function for use in PyTorch

---
+++ s2fft/utils/torch_wrapper.py
@@ -0,0 +1,172 @@
+"""
+Utilities for wrapping JAX functions for use in PyTorch.
+
+Based on Gist by Matt Johnson at
+https://gist.github.com/mattjj/e8b51074fed081d765d2f3ff90edf0e9
+
+and jax2torch package by Phil Wang
+https://github.com/lucidrains/jax2torch
+
+which is released under a MIT license
+
+Copyright (c) 2021 Phil Wang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from functools import wraps
+from inspect import signature
+from typing import Any, Callable, TypeVar
+
+import jax
+import jax.dlpack
+import torch
+import torch.utils.dlpack
+from jax.tree import map as tree_map
+
+T = TypeVar("T")
+PyTree = dict[Any, "PyTree"] | list["PyTree"] | tuple["PyTree"] | T
+
+
+def jax_array_to_torch_tensor(jax_array: jax.Array) -> torch.Tensor:
+    """
+    Convert from JAX array to Torch tensor via mutual DLPack support.
+
+    Args:
+       jax_array: JAX array to convert.
+
+    Returns:
+       Torch tensor object with equivalent data to `jax_array`.
+
+    """
+    return torch.utils.dlpack.from_dlpack(jax.dlpack.to_dlpack(jax_array))
+
+
+def torch_tensor_to_jax_array(torch_tensor: torch.Tensor) -> jax.Array:
+    """
+    Convert from Torch tensor to JAX array via mutual DLPack support.
+
+    Args:
+       torch_tensor: Torch tensor to convert.
+
+    Returns:
+       JAX array object with equivalent data to `torch_tensor`.
+
+    """
+    # JAX currently only support DLPack arrays with trivial strides so force torch
+    # tensor to be contiguous before DLPack conversion
+    # https://github.com/google/jax/issues/8082
+    torch_tensor = torch_tensor.contiguous()
+    return jax.dlpack.from_dlpack(torch.utils.dlpack.to_dlpack(torch_tensor))
+
+
+def tree_map_jax_array_to_torch_tensor(
+    jax_pytree: PyTree[jax.Array],
+) -> PyTree[torch.Tensor]:
+    """
+    Convert from a pytree with JAX arrays to corresponding pytree with Torch tensors.
+
+    Args:
+       jax_pytree: Pytree of JAX arrays or non-array values.
+
+    Returns:
+       Pytree with equivalent structure but any JAX arrays mapped to Torch tensors.
+
+    """
+    return tree_map(
+        lambda t: jax_array_to_torch_tensor(t) if isinstance(t, jax.Array) else t,
+        jax_pytree,
+    )
+
+
+def tree_map_torch_tensor_to_jax_array(
+    torch_pytree: PyTree[torch.Tensor],
+) -> PyTree[jax.Array]:
+    """
+    Convert from a pytree with Torch tensors to corresponding pytree with JAX arrays.
+
+    Args:
+       torch_pytree: Pytree of Torch tensorss or non-array values.
+
+    Returns:
+       Pytree with equivalent structure but any Torch tensors mapped to JAX arrays.
+
+    """
+    return tree_map(
+        lambda t: torch_tensor_to_jax_array(t) if isinstance(t, torch.Tensor) else t,
+        torch_pytree,
+    )
+
+
+def wrap_as_torch_function(
+    jax_function: Callable, differentiable_argnames: tuple[str] = ()
+) -> Callable:
+    """
+    Wrap a function implemented using JAX API to be callable within Torch.
+
+    Deals with conversion of argument(s) from JAX array(s) to Torch tensor(s), and of
+    return value(s) from JAX array(s) to Torch tensor(s), as well as recording
+    context needed to compute reverse-mode gradients in Torch using JAX automatic
+    differentiation support if differentiable arguments are present.
+
+    Args:
+        jax_function: JAX function to wrap.
+        differentiable_argnames: Names of arguments of `jax_function` which function
+           output(s) are differentiable with respect to, and gradients should be
+           compute with respect to in Torch backwards pass.
+
+    Returns:
+        Wrapped function callable from Torch.
+
+    """
+    sig = signature(jax_function)
+    if differentiable_argnames is not None:
+        for argname in differentiable_argnames:
+            if argname not in sig.parameters:
+                msg = f"{argname} passed is not a valid argument to {jax_function}"
+                raise ValueError(msg)
+
+    @wraps(jax_function)
+    def torch_function(*args, **kwargs):
+        bound_args = sig.bind(*args, **kwargs)
+        bound_args.apply_defaults()
+        differentiable_args = tuple(
+            bound_args.arguments[argname] for argname in differentiable_argnames
+        )
+
+        def jax_function_diff_args_only(*differentiable_args):
+            for key, value in zip(differentiable_argnames, differentiable_args):
+                bound_args.arguments[key] = value
+            return jax_function(*bound_args.args, **bound_args.kwargs)
+
+        class WrappedJaxFunction(torch.autograd.Function):
+            @staticmethod
+            def forward(ctx, *args):
+                args = tree_map_torch_tensor_to_jax_array(args)
+                primals_out, ctx.vjp = jax.vjp(jax_function_diff_args_only, *args)
+                return tree_map_jax_array_to_torch_tensor(primals_out)
+
+            @staticmethod
+            def backward(ctx, *grad_outputs):
+                grad_outputs = tree_map_torch_tensor_to_jax_array(grad_outputs)
+                return tree_map_jax_array_to_torch_tensor(ctx.vjp(*grad_outputs))
+
+        return WrappedJaxFunction.apply(*differentiable_args)
+
+    return torch_function
