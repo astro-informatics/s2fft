2d2b617f5607533c7b71e0ddb579487021a8e227
willGraham01 <ccaegra@ucl.ac.uk>
2026-03-05 11:08:54 +0000

    Remove now-duplicate test

--- tests/test_spherical_precompute.py
+++ tests/test_spherical_precompute.py
@@ -5,12 +5,10 @@ import numpy as np
 import pytest
 import torch
 
-from s2fft import inverse as precise_inverse
 from s2fft.base_transforms import spherical as base
 from s2fft.precompute_transforms import construct as c
 from s2fft.precompute_transforms.spherical import _kernel_functions, forward, inverse
 from s2fft.sampling import s2_samples as samples
-from s2fft.utils._dtype_association import compatible_cmplx_dtype
 
 jax.config.update("jax_enable_x64", True)
 
@@ -368,86 +366,3 @@ def test_inverse_transform_unrecognised_method_raises():
     flm = np.zeros(samples.flm_shape(L))
     with pytest.raises(ValueError, match=f"{method} not recognised"):
         inverse(flm, L, method=method)
-
-
-@pytest.mark.parametrize("sampling", sampling_to_test)
-@pytest.mark.parametrize("reality", reality_to_test)
-# @pytest.mark.parametrize("method", methods_to_test)
-def test_forward_lower_precision(
-    flm_generator,
-    sampling: str,
-    reality: bool,
-    L: int = 64,
-    spin: int = 0,
-    method: str = "jax",
-    recursion: str = "auto",
-    nside: int | None = None,
-):
-    """
-    Verify that flm coefficients inherit the dtype of the input arrays.
-
-    Test is run as a matrix across:
-    - sampling (to ensure there are no code-paths that are still forcing array creation with a fixed dtype)
-    - reality (effectively handles the two cases for real signals and complex signals)
-    - FIXME method (ensure that dtype behaviour occurs for all of the numpy / jax / torch paths)
-    """
-    flm, kernel = get_flm_and_kernel(
-        flm_generator,
-        L,
-        spin,
-        sampling,
-        reality,
-        method,
-        recursion,
-        forward=True,
-        nside=nside,
-    )
-    f = precise_inverse(
-        flm,
-        L=L,
-        spin=spin,
-        nside=nside,
-        sampling=sampling,
-        reality=reality,
-        method=method,
-    )
-    flm_recovered_long_dtype = forward(
-        f=f,
-        L=L,
-        spin=spin,
-        nside=nside,
-        kernel=kernel,
-        sampling=sampling,
-        reality=reality,
-        method=method,
-    )
-    round_trip_error_long_dtype = abs(flm - flm_recovered_long_dtype).max()
-    long_dtype_error_oom = np.round(np.log10(round_trip_error_long_dtype))
-
-    short_dtype = "float32" if reality else "complex64"
-    f_lower_precision = f.astype(short_dtype)
-    kernel_lower_precision = kernel.astype(short_dtype)
-    expected_short_flm_type = compatible_cmplx_dtype(f_lower_precision)
-
-    flm_recovered_short_dtype = forward(
-        f=f_lower_precision,
-        L=L,
-        spin=spin,
-        nside=nside,
-        kernel=kernel_lower_precision,
-        sampling=sampling,
-        reality=reality,
-        method=method,
-    )
-    round_trip_error_short_dtype = abs(flm - flm_recovered_short_dtype).max()
-    short_dtype_error_oom = np.round(np.log10(round_trip_error_short_dtype))
-
-    # Confirm that the output inherits the lower precision dtype
-    assert flm_recovered_short_dtype.dtype == expected_short_flm_type
-
-    # Naive expectations for the error. 1/2 precision ~= 1/2 the error OOMagnitude.
-    # Allow a -/+1 margin for near-misses during rounding and taking log.
-    assert (
-        long_dtype_error_oom <= 2 * short_dtype_error_oom
-        or long_dtype_error_oom == pytest.approx(2 * short_dtype_error_oom, abs=1)
-    )
