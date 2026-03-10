2ad49855ae59f81960708756128417f24f40e0b2
willGraham01 <ccaegra@ucl.ac.uk>
2026-03-10 12:08:33 +0000

    Wrap everything into a single parametrised test

--- tests/test_transforms_lower_precision.py
+++ tests/test_transforms_lower_precision.py
@@ -11,114 +11,17 @@ jax.config.update("jax_enable_x64", True)
 
 
 @pytest.mark.parametrize("sampling", ["mw", "mwss", "gl", "dh", "healpix"])
-@pytest.mark.parametrize("reality", [True, False])
+@pytest.mark.parametrize("fwd", [True, False], ids=("forward", "inverse"))
+@pytest.mark.parametrize(
+    "downsample_kernel", [True, False], ids=("downsample kernel", "full kernel")
+)
 @pytest.mark.parametrize("method", ["jax", "torch"])
-@pytest.mark.parametrize("downsample_kernel", [True, False])
-def test_forward_lower_precision(
+@pytest.mark.parametrize(
+    "reality", [True, False], ids=("real signal", "complex signal")
+)
+def test_lower_precision_transforms(
     get_flm_and_precompute_kernel,
-    downsample_kernel: bool,
-    sampling: str,
-    method: str,
-    reality: bool,
-    L: int = 64,
-    spin: int = 0,
-    recursion: str = "auto",
-):
-    """
-    Verify that flm coefficients inherit the dtype of the input arrays.
-
-    Test is run as a matrix across:
-    - sampling (to ensure there are no code-paths that are still forcing array creation with a fixed dtype)
-    - reality (effectively handles the two cases for real signals and complex signals)
-    - method (ensure that dtype behaviour occurs for all of the jax / torch paths)
-    - downsample_kernel (checks that dtype down-casting still occurs even if the user passes in a higher-precision kernel)
-
-    Verification takes two forms:
-    - A confirmation that the output array has the expected dtype (based on the signal dtype input)
-    - A check that the round-trip error, relative to the full-precision calculation, is within the correct order of magnitude.
-    """
-    nside = L // 2 if sampling == "healpix" else None
-
-    flm, kernel = get_flm_and_precompute_kernel(
-        L,
-        spin,
-        sampling,
-        reality,
-        method,
-        recursion,
-        forward=True,
-        nside=nside,
-    )
-    f = precise_inverse(
-        flm,
-        L=L,
-        spin=spin,
-        nside=nside,
-        sampling=sampling,
-        reality=reality,
-        method=method,
-    )
-    flm_recovered_long = forward(
-        f,
-        L=L,
-        spin=spin,
-        nside=nside,
-        kernel=kernel,
-        sampling=sampling,
-        reality=reality,
-        method=method,
-    )
-
-    short_dtype = "float32" if reality else "complex64"
-    casting_method = "astype"
-    if method == "torch":
-        short_dtype = getattr(torch, short_dtype)
-        casting_method = "to"
-
-    f_lower_precision = getattr(f, casting_method)(short_dtype)
-    kernel_to_use = (
-        getattr(kernel, casting_method)(short_dtype) if downsample_kernel else kernel
-    )
-    expected_short_flm_type = compatible_cmplx_dtype(f_lower_precision)
-
-    flm_recovered_short = forward(
-        f_lower_precision,
-        L=L,
-        spin=spin,
-        nside=nside,
-        kernel=kernel_to_use,
-        sampling=sampling,
-        reality=reality,
-        method=method,
-    )
-    flm_recovered_short_dtype = flm_recovered_short.dtype
-    if method == "torch":
-        flm = torch.Tensor(flm)
-        flm_recovered_short_dtype = str(flm_recovered_short_dtype)
-
-    round_trip_error_long_dtype = abs(flm - flm_recovered_long).max()
-    long_dtype_error_oom = np.round(np.log10(round_trip_error_long_dtype))
-
-    round_trip_error_short_dtype = abs(flm - flm_recovered_short).max()
-    short_dtype_error_oom = np.round(np.log10(round_trip_error_short_dtype))
-
-    # Confirm that the output inherits the lower precision dtype
-    assert flm_recovered_short_dtype == expected_short_flm_type
-
-    # Naive expectations for the error. 1/2 precision ~= 1/2 the error OOMagnitude.
-    # Allow a -/+1 margin for near-misses during rounding and taking log.
-    assert (
-        long_dtype_error_oom <= 2 * short_dtype_error_oom
-        or long_dtype_error_oom == pytest.approx(2 * short_dtype_error_oom, abs=1)
-    )
-
-
-@pytest.mark.parametrize("sampling", ["mw", "mwss", "gl", "dh", "healpix"])
-@pytest.mark.parametrize("reality", [True, False])
-@pytest.mark.parametrize("method", ["jax", "torch"])
-@pytest.mark.parametrize("downsample_kernel", [True, False])
-def test_inverse_lower_precision(
-    get_flm_and_precompute_kernel,
+    fwd: bool,
     downsample_kernel: bool,
     sampling: str,
     method: str,
@@ -131,6 +34,8 @@ def test_inverse_lower_precision(
     Verify that signal arrays inherit the dtype of the input harmonic coefficient arrays.
 
     Test is run as a matrix across:
+    - fwd (to test both the forward [True] and inverse [False] transforms)
+    - downsample_kernel (to test that the kernel dtype is ignored when deciding on the output dtype)
     - sampling (to ensure there are no code-paths that are still forcing array creation with a fixed dtype)
     - reality (effectively handles the two cases for real signals and complex signals). Note that for the inverse transform, the output is returned as a floatXX array if reality is set to True, however this conversion doesn't
     actually occur until after all computations have been conducted.
@@ -138,72 +43,101 @@ def test_inverse_lower_precision(
 
     Verification takes two forms:
     - A confirmation that the output array has the expected dtype (based on the signal dtype input)
-    - A check that the round-trip error, relative to the full-precision calculation, is within the correct order of magnitude.
+    - A check on the orders of magnitude of the round trip error for both the long-precision and short-precision computations.
+      Specifically, it is checked that the OoM for the short precision calculation is half that of the long-precision
+      calculation, plus or minus 1 (to allow for rounding and near-misses).
     """
     nside = L // 2 if sampling == "healpix" else None
+    common_args = {
+        "L": L,
+        "spin": spin,
+        "nside": nside,
+        "sampling": sampling,
+        "reality": reality,
+        "method": method,
+    }
 
+    # Generate flm coefficients and signal to use as "baseline truths"
     flm, kernel = get_flm_and_precompute_kernel(
-        L,
-        spin,
-        sampling,
-        reality,
-        method,
-        recursion,
-        forward=False,
-        nside=nside,
+        recursion=recursion,
+        forward=fwd,
+        **common_args,
     )
-    trusted_signal = precise_inverse(
-        flm,
-        L=L,
-        nside=nside,
-        sampling=sampling,
-        method=method,
-        reality=reality,
-    )
+    f = precise_inverse(flm, **common_args)
 
-    f_recovered_long = inverse(
-        flm,
-        L=L,
-        kernel=kernel,
-        sampling=sampling,
-        reality=reality,
-        method=method,
-        nside=nside,
-    )
+    # Establish;
+    # - which array is being transformed
+    # - which transform method to call
+    # - the 'true values' that should be recovered
+    # - the down-cast input array dtype & the lower-precision input
+    # - the expected dtype of the lower-precision calculation output
+    casting_method = "astype" if method != "torch" else "to"
+    if fwd:
+        to_transform = f
+        transform_direction = forward
+        true_values = flm
 
-    short_dtype = "complex64"
-    expected_short_f_type = "float32" if reality else "complex64"
-    casting_method = "astype"
+        short_dtype = "float32" if reality else "complex64"
+        if method == "torch":
+            short_dtype = getattr(torch, short_dtype)
+
+        to_transform_lower_precision = getattr(to_transform, casting_method)(
+            short_dtype
+        )
+        # forward transform should result in complex array output,
+        # even if the signal is real.
+        expected_short_dtype = compatible_cmplx_dtype(to_transform_lower_precision)
+    else:
+        to_transform = flm
+        transform_direction = inverse
+        true_values = f
+
+        short_dtype = "complex64"
+        # Inverse transform should cast to real arrays if reality is specified.
+        expected_short_dtype = "float32" if reality else "complex64"
+        if method == "torch":
+            short_dtype = getattr(torch, short_dtype)
+            expected_short_dtype = getattr(torch, expected_short_dtype)
+            to_transform = torch.Tensor(to_transform)
+
+        to_transform_lower_precision = getattr(to_transform, casting_method)(
+            short_dtype
+        )
+
+    # Torch things to avoid operation errors when comparing to numpy arrays
     if method == "torch":
-        short_dtype = getattr(torch, short_dtype)
-        casting_method = "to"
-        expected_short_f_type = getattr(torch, expected_short_f_type)
-        flm = torch.Tensor(flm)
+        true_values = torch.Tensor(true_values)
 
-    flm_lower_precision = getattr(flm, casting_method)(short_dtype)
+    # Down-cast the kernel if instructed to do so
     kernel_to_use = (
         getattr(kernel, casting_method)(short_dtype) if downsample_kernel else kernel
     )
 
-    f_recovered_short = inverse(
-        flm_lower_precision,
-        L=L,
-        spin=spin,
-        nside=nside,
-        kernel=kernel_to_use,
-        sampling=sampling,
-        reality=reality,
-        method=method,
+    # Perform transform in long precision
+    long_precision_result = transform_direction(
+        to_transform,
+        kernel=kernel,
+        **common_args,
     )
-    f_recovered_short_dtype = f_recovered_short.dtype
+    # Perform transform in short precision, possibly with down-cast kernel
+    short_precision_result = transform_direction(
+        to_transform_lower_precision,
+        kernel=kernel_to_use,
+        **common_args,
+    )
 
-    error_long_dtype = abs(trusted_signal - f_recovered_long).max()
-    long_dtype_error_oom = np.round(np.log10(error_long_dtype))
+    # Confirm that the output inherits the lower precision dtype
+    short_result_dtype = short_precision_result.dtype
+    assert str(short_result_dtype) == str(expected_short_dtype)
 
-    error_short_dtype = abs(trusted_signal - f_recovered_short).max()
-    short_dtype_error_oom = np.round(np.log10(error_short_dtype))
+    # Check expectations for the error. 1/2 precision ~= 1/2 the error OOMagnitude.
+    # Allow a -/+1 margin for near-misses during rounding and taking log.
+    round_trip_error_long_dtype = abs(true_values - long_precision_result).max()
+    long_dtype_error_oom = np.round(np.log10(round_trip_error_long_dtype))
 
-    assert f_recovered_short_dtype == expected_short_f_type
+    round_trip_error_short_dtype = abs(true_values - short_precision_result).max()
+    short_dtype_error_oom = np.round(np.log10(round_trip_error_short_dtype))
+
     assert (
         long_dtype_error_oom <= 2 * short_dtype_error_oom
         or long_dtype_error_oom == pytest.approx(2 * short_dtype_error_oom, abs=1)
