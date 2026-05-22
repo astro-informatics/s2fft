4fbdded26bd4c64f07cce6d10fb38ebfeb6b5705
Matt Graham <matthew.m.graham@gmail.com>
2026-05-22 13:40:22 +0100

    Remove unneeded special case for CC rule with n_theta  = 2

--- s2fft/utils/quadrature.py
+++ s2fft/utils/quadrature.py
@@ -420,10 +420,10 @@ def quad_weights_cc_theta_only(L: int, xp: ModuleType = np) -> np.ndarray:
 
     n_theta = samples.ntheta(L, "cc")
 
+    # Computing RFFT below with n=0 will fail so explicitly return
+    # weights for single point case
     if n_theta == 1:
         return xp.array([2.0])
-    elif n_theta == 2:
-        return xp.array([1.0, 1.0])
 
     n = n_theta - 1
     w = _fejer_second_rule_rfft_weights(n, xp)
