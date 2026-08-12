52bcc304bdfd3536b24e272b86993076d203ac67
Matt Graham <matthew.m.graham@gmail.com>
2026-08-12 15:21:27 +0100

    Ensure polynomial quadrature tests check exactness at critical number of points

--- tests/test_quadrature.py
+++ tests/test_quadrature.py
@@ -56,9 +56,50 @@ def test_quadrature_exceptions():
             quadrature.quad_weights(0, sampling)
 
 
+def _bandlimit_from_n_theta(n_theta, rule):
+    # Compute bandlimit L such that ntheta(L, rule) in {n_theta, n_theta + 1}
+    match rule:
+        case "mw" | "gl":
+            return n_theta
+        case "f2" | "cc":
+            return n_theta // 2 + 1
+        case "mwss":
+            return n_theta - 1
+        case "dh":
+            return (n_theta + 1) // 2
+
+
+def _min_n_points_exact(rule, polynomial_degree):
+    # Minimum number of quadrature points to exactly integrate polynomial of
+    # specified degree for a given rule
+    match rule:
+        case "gl":
+            return polynomial_degree // 2 + 1
+        case "f2" | "cc":
+            return polynomial_degree
+        case "dh" | "mw":
+            return polynomial_degree + 1
+        case "mwss":
+            return polynomial_degree + 2
+
+
+def _min_n_points(rule):
+    # Rules including both poles need a minimum of 2 points
+    if rule in (
+        samples.INCLUDES_NORTH_POLE_SCHEMES & samples.INCLUDES_SOUTH_POLE_SCHEMES
+    ):
+        return 2
+    else:
+        return 1
+
+
 def check_quadrature_rule(f_and_integral, rule, n_points, quadrature_module, tol=1e-12):
     f, true_integral = f_and_integral
-    thetas = samples.thetas(n_points, sampling=rule)
+    L = _bandlimit_from_n_theta(n_points, rule)
+    # As ntheta may be constrained to be an odd or even number given L,
+    # we allow that ntheta may be equal to n_points or one more
+    assert samples.ntheta(L, rule) in (n_points, n_points + 1)
+    thetas = samples.thetas(L, sampling=rule)
     xs = np.cos(thetas)
     weights = {
         "cc": quadrature_module.quad_weights_cc_theta_only,
@@ -67,24 +108,36 @@ def check_quadrature_rule(f_and_integral, rule, n_points, quadrature_module, tol
         "mwss": quadrature_module.quad_weights_mwss_theta_only,
         "gl": quadrature_module.quad_weights_gl_theta_only,
         "dh": quadrature_module.quad_weights_dh_theta_only,
-    }[rule](n_points)
+    }[rule](L)
     quad_integral = (f(xs) * weights).sum()
     assert abs(quad_integral - true_integral) < tol
 
 
 @pytest.mark.parametrize(
-    "f_and_integral",
+    "f_and_integral_and_degree",
     [
-        (lambda x: x, 0.0),
-        (lambda x: x**2, 2 / 3),
-        (lambda x: x**3 - 2 * x**2 + x - 1, -10 / 3),
-        (lambda x: x**4 - x**2, -4 / 15),
+        (lambda x: 0.0, 0.0, 0),
+        (lambda x: x + 1, 2.0, 1),
+        (lambda x: x**2, 2 / 3, 2),
+        (lambda x: x**3 - 2 * x**2 + x - 1, -10 / 3, 3),
+        (lambda x: x**4 - x**2, -4 / 15, 4),
+        (lambda x: x**8 - x**4 + x**3 - 2 * x**2 + x - 1, -158 / 45, 8),
     ],
 )
 @pytest.mark.parametrize("rule", ["cc", "f2", "mw", "mwss", "gl", "dh"])
-@pytest.mark.parametrize("n_points", [6, 8, 16])
+@pytest.mark.parametrize("n_points_offset", [0, 1, 2, 4])
 @pytest.mark.parametrize("quadrature_module", [quadrature, quadrature_jax])
-def test_quadrature_polynomial(f_and_integral, rule, n_points, quadrature_module):
+def test_quadrature_polynomial(
+    f_and_integral_and_degree, rule, n_points_offset, quadrature_module
+):
+    *f_and_integral, polynomial_degree = f_and_integral_and_degree
+    # Number of quadrature points defined relative to minimum number
+    # required to exactly integrate polynomial of specified degree
+    # to ensure we test this minimum (when n_points_offset == 0)
+    n_points = max(
+        _min_n_points_exact(rule, polynomial_degree) + n_points_offset,
+        _min_n_points(rule),
+    )
     check_quadrature_rule(f_and_integral, rule, n_points, quadrature_module)
 
 
